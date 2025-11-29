06b7cef0374448a047e3acfdd8d6a61a385d9453
botigi9404 <126343755+botigi9404@users.noreply.github.com>
2025-11-29 12:44:25 -0600

    added helmet and cors to server.js; built config/dbconfig.js

--- server.js
+++ server.js
@@ -3,6 +3,29 @@ const express = require('express')
 const server = express()
 const PORT = process.env.PORT || 2025
 
+/* HANDLE SECURITY .. IMPORTING HELMET AND CORS*/
+const helmet = require('helmet')
+const cors = require('cors')
+
+/* ADDS THE LYER FOR SECURITY FOR IMGS AND STATICS */
+//server.use(helmet()) 
+/* CONFIGURING HELMET */
+server.use(helmet.contentSecurityPolicy({
+    useDefaults: true,
+    crossOriginResourcePolicy: false,
+    crossOriginEmbedderPolicy: false,
+    directives: {
+        "img-src": ["'self'", "https: data"],
+        "scriptSrc": ["'self'", "cdn.jsdelivr.net"]  
+    }
+}))
+
+server.use(cors())
+server.use(express.json())
+server.use(express.urlencoded({ extended: true}))
+
+
+
 
 
 
