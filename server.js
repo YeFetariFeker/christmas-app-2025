61cac313b768c402de0deb222936d8ecfd976e05
botigi9404 <126343755+botigi9404@users.noreply.github.com>
2025-11-29 10:58:57 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+/* BUILD SERVER */
+const express = require('express')
+const server = express()
+const PORT = process.env.PORT || 2025
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* BUILD A SERVER THAT CONNECTS TO GIT BASH SERVER */
+server.listen(PORT, ()=> console.log('Merry Christmas Everyone Dec 2025!!!🎄🎅🎁❄️'))
